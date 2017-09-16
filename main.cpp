33fa1edebbf6e854e2c95ee825bb2d3c46cc076a
0aix <brianx2000@gmail.com>
2017-09-16 19:44:51 -0400

    cave explorer

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 #include <glm/gtc/type_ptr.hpp>
 
 #include <chrono>
+#include <fstream>
 #include <iostream>
 #include <stdexcept>
 
@@ -15,7 +16,7 @@ static GLuint link_program(GLuint vertex_shader, GLuint fragment_shader);
 int main(int argc, char **argv) {
 	//Configuration:
 	struct {
-		std::string title = "Game1: Text/Tiles";
+		std::string title = "Game1: Cave Explorer";
 		glm::uvec2 size = glm::uvec2(640, 480);
 	} config;
 
@@ -77,7 +78,7 @@ int main(int argc, char **argv) {
 	}
 
 	//Hide mouse cursor (note: showing can be useful for debugging):
-	SDL_ShowCursor(SDL_DISABLE);
+	//SDL_ShowCursor(SDL_DISABLE);
 
 	//------------ opengl objects / game assets ------------
 
@@ -87,7 +88,7 @@ int main(int argc, char **argv) {
 
 	{ //load texture 'tex':
 		std::vector< uint32_t > data;
-		if (!load_png("elements.png", &tex_size.x, &tex_size.y, &data, LowerLeftOrigin)) {
+		if (!load_png("roguelikeSheet_transparent.png", &tex_size.x, &tex_size.y, &data, LowerLeftOrigin)) {
 			std::cerr << "Failed to load texture." << std::endl;
 			exit(1);
 		}
@@ -142,17 +143,17 @@ int main(int argc, char **argv) {
 
 		//look up attribute locations:
 		program_Position = glGetAttribLocation(program, "Position");
-		if (program_Position == -1U) throw std::runtime_error("no attribute named Position");
+		if (program_Position == -1) throw std::runtime_error("no attribute named Position");
 		program_TexCoord = glGetAttribLocation(program, "TexCoord");
-		if (program_TexCoord == -1U) throw std::runtime_error("no attribute named TexCoord");
+		if (program_TexCoord == -1) throw std::runtime_error("no attribute named TexCoord");
 		program_Color = glGetAttribLocation(program, "Color");
-		if (program_Color == -1U) throw std::runtime_error("no attribute named Color");
+		if (program_Color == -1) throw std::runtime_error("no attribute named Color");
 
 		//look up uniform locations:
 		program_mvp = glGetUniformLocation(program, "mvp");
-		if (program_mvp == -1U) throw std::runtime_error("no uniform named mvp");
+		if (program_mvp == -1) throw std::runtime_error("no uniform named mvp");
 		program_tex = glGetUniformLocation(program, "tex");
-		if (program_tex == -1U) throw std::runtime_error("no uniform named tex");
+		if (program_tex == -1) throw std::runtime_error("no uniform named tex");
 	}
 
 	//vertex buffer:
@@ -191,21 +192,55 @@ int main(int argc, char **argv) {
 		glm::vec2 rad = glm::vec2(0.5f);
 	};
 
-
-	auto load_sprite = [](std::string const &name) -> SpriteInfo {
-		SpriteInfo info;
-		//TODO: look up sprite name in table of sprite infos
-		return info;
+	float coord[] = {
+		17.0f, 17.0f, // dirt tile - 0
+		731.0f, 187, // track - 1
+		731.0f, 204.0f, // side track - 2
+		731.0f, 340.0f, // gold bars - 3
+		221.0f, 17.0f, // dirt wall - 4
+		833.0f, 204.0f, // side cart - 5
+		867.0f, 204.0f, // cart - 6
+		697.0f, 340.0f, // logs - 7
+		833.0f, 153.0f, // side cart w/ gold - 8
+		867.0f, 153.0f // cart w/ gold 9
 	};
 
+	SpriteInfo sprites[10];
+	for (int i = 0; i < 10; i++) {
+		sprites[i].min_uv.x = coord[2 * i] / 968.0f;
+		sprites[i].min_uv.y = coord[2 * i + 1] / 526.0f;
+		sprites[i].max_uv.x = (coord[2 * i] + 16.0f) / 968.0f;
+		sprites[i].max_uv.y = (coord[2 * i + 1] + 16.0f) / 526.0f;
+	}
 
 	//------------ game state ------------
-
-	glm::vec2 mouse = glm::vec2(0.0f, 0.0f); //mouse position in [-1,1]x[-1,1] coordinates
+	const int length = 11;
+	const int width = 11;
+	int grid[length][width] = {
+		{ 0, 2, 0, 2, 2, 0, 2, 2, 0, 4, 3 },
+		{ 4, 4, 1, 4, 4, 1, 4, 4, 1, 4, 0 },
+		{ 3, 4, 0, 4, 4, 1, 4, 4, 1, 4, 1 },
+		{ 0, 4, 4, 4, 4, 0, 2, 2, 0, 4, 1 },
+		{ 1, 4, 4, 4, 4, 1, 4, 4, 1, 4, 1 },
+		{ 0, 2, 2, 2, 2, 0, 4, 4, 0, 2, 0 },
+		{ 4, 4, 4, 4, 4, 1, 4, 4, 4, 4, 4 },
+		{ 3, 4, 0, 2, 2, 0, 2, 2, 0, 2, 0 },
+		{ 0, 4, 1, 4, 4, 4, 4, 4, 1, 4, 3 },
+		{ 1, 4, 1, 4, 4, 4, 3, 4, 1, 4, 4 },
+		{ 0, 2, 0, 2, 2, 2, 0, 2, 0, 2, 0 }
+	};
+	bool lit[length][width] = {};
+	const float speed = 5.0f;
+	glm::uvec2 pos = glm::uvec2(5, 5);
+	glm::vec2 cart = glm::vec2(0, 0);
+	float loc = 0.0f;
+	int state = 0; // 0 - horizontal, 1 - left, 2 - right, 3 - vertical, 4 - up, 5 - down, +6 - gold and win
+	int num_gold_left = 5;
+	lit[pos.y][pos.x] = true;
 
 	struct {
 		glm::vec2 at = glm::vec2(0.0f, 0.0f);
-		glm::vec2 radius = glm::vec2(10.0f, 10.0f);
+		glm::vec2 radius = glm::vec2(5.5f);
 	} camera;
 	//correct radius for aspect ratio:
 	camera.radius.x = camera.radius.y * (float(config.size.x) / float(config.size.y));
@@ -217,12 +252,79 @@ int main(int argc, char **argv) {
 		static SDL_Event evt;
 		while (SDL_PollEvent(&evt) == 1) {
 			//handle input:
-			if (evt.type == SDL_MOUSEMOTION) {
-				mouse.x = (evt.motion.x + 0.5f) / float(config.size.x) * 2.0f - 1.0f;
-				mouse.y = (evt.motion.y + 0.5f) / float(config.size.y) *-2.0f + 1.0f;
-			} else if (evt.type == SDL_MOUSEBUTTONDOWN) {
-			} else if (evt.type == SDL_KEYDOWN && evt.key.keysym.sym == SDLK_ESCAPE) {
-				should_quit = true;
+			if (evt.type == SDL_KEYDOWN) {
+				if (evt.key.keysym.sym == SDLK_ESCAPE) {
+					should_quit = true;
+				}
+				if (state == 0 || state == 3) {
+					if (evt.key.keysym.sym == SDLK_LEFT) {
+						state = 0;
+						if (pos.x > 0 && grid[pos.y][pos.x - 1] == 2) {
+							pos.x--;
+							loc = cart.x - 1.0f;
+							state = 1;
+							lit[pos.y][pos.x] = true;
+						}
+					} else if (evt.key.keysym.sym == SDLK_RIGHT) {
+						state = 0;
+						if (pos.x < width - 1 && grid[pos.y][pos.x + 1] == 2) {
+							pos.x++;
+							loc = cart.x + 1.0f;
+							state = 2;
+							lit[pos.y][pos.x] = true;
+						}
+					} else if (evt.key.keysym.sym == SDLK_UP) {
+						state = 3;
+						if (pos.y > 0 && grid[pos.y - 1][pos.x] == 1) {
+							pos.y--;
+							loc = cart.y + 1.0f;
+							state = 4;
+							lit[pos.y][pos.x] = true;
+						}
+					} else if (evt.key.keysym.sym == SDLK_DOWN) {
+						state = 3;
+						if (pos.y < length - 1 && grid[pos.y + 1][pos.x] == 1) {
+							pos.y++;
+							loc = cart.y - 1.0f;
+							state = 5;
+							lit[pos.y][pos.x] = true;
+						}
+					} else if (evt.key.keysym.sym == SDLK_SPACE) {
+						if (pos.x > 0 && grid[pos.y][pos.x - 1] == 3) {
+							if (std::chrono::high_resolution_clock::now().time_since_epoch().count() % num_gold_left == 0) {
+								std::cout << "You win!" << std::endl;
+								state += 6;
+							} else {
+								grid[pos.y][pos.x - 1] = 7;
+								num_gold_left--;
+							}
+						} else if (pos.x < width - 1 && grid[pos.y][pos.x + 1] == 3) {
+							if (std::chrono::high_resolution_clock::now().time_since_epoch().count() % num_gold_left == 0) {
+								std::cout << "You win!" << std::endl;
+								state += 6;
+							} else {
+								grid[pos.y][pos.x + 1] = 7;
+								num_gold_left--;
+							}
+						} else if (pos.y > 0 && grid[pos.y - 1][pos.x] == 3) {
+							if (std::chrono::high_resolution_clock::now().time_since_epoch().count() % num_gold_left == 0) {
+								std::cout << "You win!" << std::endl;
+								state += 6;
+							} else {
+								grid[pos.y - 1][pos.x] = 7;
+								num_gold_left--;
+							}
+						} else if (pos.y < length - 1 && grid[pos.y + 1][pos.x] == 3) {
+							if (std::chrono::high_resolution_clock::now().time_since_epoch().count() % num_gold_left == 0) {
+								std::cout << "You win!" << std::endl;
+								state += 6;
+							} else {
+								grid[pos.y + 1][pos.x] = 7;
+								num_gold_left--;
+							}
+						}
+					}
+				}
 			} else if (evt.type == SDL_QUIT) {
 				should_quit = true;
 				break;
@@ -236,11 +338,95 @@ int main(int argc, char **argv) {
 		previous_time = current_time;
 
 		{ //update game state:
-			(void)elapsed;
+			if (state == 1) {
+				cart.x -= elapsed * speed;
+				while (cart.x < loc) {
+					if (grid[pos.y][pos.x] == 0) {
+						cart.x = loc;
+						state = 0;
+						if (pos.x > 0 && grid[pos.y][pos.x - 1] == 3) {
+							lit[pos.y][pos.x - 1] = true;
+						} else if (pos.x < width - 1 && grid[pos.y][pos.x + 1] == 3) {
+							lit[pos.y][pos.x + 1] = true;
+						} else if (pos.y > 0 && grid[pos.y - 1][pos.x] == 3) {
+							lit[pos.y - 1][pos.x] = true;
+						} else if (pos.y < length - 1 && grid[pos.y + 1][pos.x] == 3) {
+							lit[pos.y + 1][pos.x] = true;
+						}
+					} else {
+						pos.x--;
+						loc -= 1.0f;
+						lit[pos.y][pos.x] = true;
+					}
+				}
+			} else if (state == 2) {
+				cart.x += elapsed * speed;
+				while (cart.x > loc) {
+					if (grid[pos.y][pos.x] == 0) {
+						cart.x = loc;
+						state = 0;
+						if (pos.x > 0 && grid[pos.y][pos.x - 1] == 3) {
+							lit[pos.y][pos.x - 1] = true;
+						} else if (pos.x < width - 1 && grid[pos.y][pos.x + 1] == 3) {
+							lit[pos.y][pos.x + 1] = true;
+						} else if (pos.y > 0 && grid[pos.y - 1][pos.x] == 3) {
+							lit[pos.y - 1][pos.x] = true;
+						} else if (pos.y < length - 1 && grid[pos.y + 1][pos.x] == 3) {
+							lit[pos.y + 1][pos.x] = true;
+						}
+					} else {
+						pos.x++;
+						loc += 1.0f;
+						lit[pos.y][pos.x] = true;
+					}
+				}
+			} else if (state == 4) {
+				cart.y += elapsed * speed;
+				while (cart.y > loc) {
+					if (grid[pos.y][pos.x] == 0) {
+						cart.y = loc;
+						state = 3;
+						if (pos.x > 0 && grid[pos.y][pos.x - 1] == 3) {
+							lit[pos.y][pos.x - 1] = true;
+						} else if (pos.x < width - 1 && grid[pos.y][pos.x + 1] == 3) {
+							lit[pos.y][pos.x + 1] = true;
+						} else if (pos.y > 0 && grid[pos.y - 1][pos.x] == 3) {
+							lit[pos.y - 1][pos.x] = true;
+						} else if (pos.y < length - 1 && grid[pos.y + 1][pos.x] == 3) {
+							lit[pos.y + 1][pos.x] = true;
+						}
+					} else {
+						pos.y--;
+						loc += 1.0f;
+						lit[pos.y][pos.x] = true;
+					}
+				}
+			} else if (state == 5) {
+				cart.y -= elapsed * speed;
+				while (cart.y < loc) {
+					if (grid[pos.y][pos.x] == 0) {
+						cart.y = loc;
+						state = 3;
+						if (pos.x > 0 && grid[pos.y][pos.x - 1] == 3) {
+							lit[pos.y][pos.x - 1] = true;
+						} else if (pos.x < width - 1 && grid[pos.y][pos.x + 1] == 3) {
+							lit[pos.y][pos.x + 1] = true;
+						} else if (pos.y > 0 && grid[pos.y - 1][pos.x] == 3) {
+							lit[pos.y - 1][pos.x] = true;
+						} else if (pos.y < length - 1 && grid[pos.y + 1][pos.x] == 3) {
+							lit[pos.y + 1][pos.x] = true;
+						}
+					} else {
+						pos.y++;
+						loc -= 1.0f;
+						lit[pos.y][pos.x] = true;
+					}
+				}
+			}
 		}
 
 		//draw output:
-		glClearColor(0.5, 0.5, 0.5, 0.0);
+		glClearColor(78.0f / 255.0f, 46 / 255.0f, 40.0f / 255.0f, 0.0);
 		glClear(GL_COLOR_BUFFER_BIT);
 		glEnable(GL_BLEND);
 		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
@@ -268,21 +454,40 @@ int main(int argc, char **argv) {
 				glm::vec2 up = glm::vec2(-right.y, right.x);
 
 				verts.emplace_back(at + right * -rad.x + up * -rad.y, glm::vec2(min_uv.x, min_uv.y), tint);
-				verts.emplace_back(verts.back());
+				verts.push_back(verts.back());
 				verts.emplace_back(at + right * -rad.x + up * rad.y, glm::vec2(min_uv.x, max_uv.y), tint);
 				verts.emplace_back(at + right *  rad.x + up * -rad.y, glm::vec2(max_uv.x, min_uv.y), tint);
 				verts.emplace_back(at + right *  rad.x + up *  rad.y, glm::vec2(max_uv.x, max_uv.y), tint);
-				verts.emplace_back(verts.back());
+				verts.push_back(verts.back());
 			};
 
+			// draw background
+			glm::vec2 at;
+			at.y = 0.5f * (length - 1);
+			for (int i = 0; i < length; i++) {
+				at.x = -0.5f * (width - 1);
+				for (int j = 0; j < width; j++) {
+					if (state >= 6 || lit[i][j]) {
+						draw_sprite(sprites[0], at);
+						if (grid[i][j] != 0)
+							draw_sprite(sprites[grid[i][j]], at);
+					} else {
+						draw_sprite(sprites[4], at);
+					}
+					at.x += 1.0f;
+				}
+				at.y -= 1.0f;
+			}
 
-			//Draw a sprite "player" at position (5.0, 2.0):
-			static SpriteInfo player = load_sprite("player"); //TODO: hoist
-			draw_sprite(player, glm::vec2(5.0, 2.0), 0.2f);
-
-			rect(glm::vec2(0.0f, 0.0f), glm::vec2(4.0f), glm::u8vec4(0xff, 0x00, 0x00, 0xff));
-			rect(mouse * camera.radius + camera.at, glm::vec2(4.0f), glm::u8vec4(0xff, 0xff, 0xff, 0x88));
-
+			// draw player
+			if (state <= 2)
+				draw_sprite(sprites[5], cart);
+			else if (state <= 5)
+				draw_sprite(sprites[6], cart);
+			else if (state <= 8)
+				draw_sprite(sprites[8], cart);
+			else
+				draw_sprite(sprites[9], cart);
 
 			glBindBuffer(GL_ARRAY_BUFFER, buffer);
 			glBufferData(GL_ARRAY_BUFFER, sizeof(Vertex) * verts.size(), &verts[0], GL_STREAM_DRAW);
